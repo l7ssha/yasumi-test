40477f5f86ab529597853cf832ea5191a0ed373d
Szymon Uglis <szymon.uglis@auto1.com>
2024-10-18 14:09:22 +0200

    Add Germany and Poland

--- src/index.php
+++ src/index.php
@@ -8,35 +8,54 @@ require_once 'vendor/autoload.php';
 
 use InitPHP\CLITable\Table;
 use Yasumi\Provider\France;
-use Yasumi\Provider\France\Moselle;
+use Yasumi\Provider\Germany;
+use Yasumi\Provider\Poland;
 use Yasumi\Yasumi;
 
-$franceProvider = Yasumi::create(France::class, 2025);
-$moselleProvider = Yasumi::create(Moselle::class, 2025);
+/**
+ * @param class-string[] $countries
+ * @return array{string, string, array<string, array<string, string>>}
+ */
+function getHolidaysMatrix(array $countries, int $year): array
+{
 
-/** @var array<string, array<string, array<string>>> $holidaysMatrix */
-$holidaysMatrix = [];
-foreach ($franceProvider->getHolidays() as $holiday) {
-    $holidaysMatrix[$holiday->format('Y-m-d')]['France'] = $holiday->getName();
+    /** @var array<string, array<string, array<string>>> $holidaysMatrix */
+    $holidaysMatrix = [];
+
+    foreach ($countries as $country) {
+        foreach (Yasumi::create($country, 2025)->getHolidays() as $holiday) {
+            $holidaysMatrix[$holiday->format('Y-m-d')][$country] = $holiday->getName();
+        }
+    }
+
+    ksort($holidaysMatrix);
+
+    return [
+        $countries,
+        $holidaysMatrix,
+    ];
 }
 
-foreach ($moselleProvider->getHolidays() as $holiday) {
-    $holidaysMatrix[$holiday->format('Y-m-d')]['Moselle'] = $holiday->getName();
+function displayTable(array $countries, array $matrix): void
+{
+    $table = new Table();
+    foreach ($matrix as $date => $holidays) {
+        $rowData = [
+            'date' => $date,
+        ];
+
+        foreach ($countries as $country) {
+            $countryOneHolidayName = $holidays[$country] ?? '';
+
+            $rowData[$country] = $countryOneHolidayName;
+        }
+
+        $table->row($rowData);
+    }
+
+    echo $table;
 }
 
-ksort($holidaysMatrix);
-
-$table = new Table();
-
-foreach ($holidaysMatrix as $date => $holidays) {
-    $franceHolidayName = $holidays['France'] ?? '';
-    $moselleHolidayName = $holidays['Moselle'] ?? '';
-
-    $table->row([
-        'date' => $date,
-        'France' => $franceHolidayName,
-        'Moselle' => $moselleHolidayName
-    ]);
-}
-
-echo $table;
+displayTable(...getHolidaysMatrix([France::class, France\Moselle::class], 2025));
+displayTable(...getHolidaysMatrix([Germany::class, Germany\Berlin::class], 2025));
+displayTable(...getHolidaysMatrix([Poland::class], 2025));
